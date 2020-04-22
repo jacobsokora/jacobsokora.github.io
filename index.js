47dba62f714ff6d6233938c538fb06ec1ce28086
Jacob <jacobsokora@gmail.com>
2020-04-22 01:00:18 -0500

    add ghostwriter stuff

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ $(() => {
 		$('#portfolio').hide();
 	});
 });
+
+var code = [ 38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 66, 65];
+var pos = 0;
+
+document.addEventListener('keydown', (e) => {
+	if (code[pos] == e.keyCode) {
+		pos += 1;
+		if (pos == code.length) {
+			pos = 0;
+		}
+	} else {
+		pos = 0;
+	}
+});
